e5552a535144cf0b470a28e8e9fba1dc7df15f5d
janelle-lr <270065505@yoobeestudent.ac.nz>
2021-05-24 14:44:01 +1200

    heehee crying

--- teamProjectTest/teamProjectTest/teamProjectTest.cpp
+++ teamProjectTest/teamProjectTest/teamProjectTest.cpp
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Hello Janelle!\n"; // Kallums code
+    std::cout << "Hello Kallum!\n"; //Janelle's code lol
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
